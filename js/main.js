9c5d0b0924a8302094425f9d28eb84d045f3cef2
davidfryling <davidfryling@gmail.com>
2020-11-30 18:12:59 -0500

    comment out html demo and add dictionaries

---
+++ js/main.js
@@ -0,0 +1,83 @@
+const user;
+const newStudent = {
+    fName : "",
+    lName : "",
+    cid : "",
+    sEmail : "",
+    pEmail : "",
+    school : "",
+    ssid : "",
+    maxCrd : ""
+};
+const newRequest = {
+    subj : "",
+    courseN : "",
+    sectN : "",
+    synN : "",
+    crdHrs : "",
+    cName : "",
+    daysTimes : "",
+    parentApp : "",
+    counsApp : "",
+    reg : "",
+    note: ""
+};
+const janeD = {
+    fName : "Jane",
+    lName : "Doe",
+    cid : "1234567",
+    sEmail : "jane.doe@reyn.org",
+    pEmail : "mama.doe@reyn.org",
+    school : "HS2",
+    ssid : "",
+    maxCrd : ""
+}
+const janeDRequest = {
+    subj : "MATH",
+    courseN : "1149",
+    sectN : "001",
+    synN : "12345",
+    crdHrs : "4",
+    cName : "Trig",
+    daysTimes : "M/W 2:00-3:00 pm",
+    parentApp : "",
+    counsApp : "",
+    reg : "",
+    note: ""
+}
+
+function 
+
+
+
+
+// below is template of each element grouping that could be rendered for reference and use in functions
+
+/* <div class="wrapper"> 
+    <h1>Page Title</h1>
+    <h3>Instructions</h3>
+</div>    
+<div class="wrapper">  
+    <!-- cell wrapper for each peice of data -->
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+        <p class="data-cell">Data</p>
+</div>
+<div class="wrapper">    
+    <button class="lg-btn h">large button</button>
+    <button class="sm-btn h">small button</button>
+</div>
+<div class="wrapper">   
+    <input class="lg-frm h" type="text" placeholder="form field">
+    <input class="sm-frm h" type="text" placeholder="form field">
+    <button class="sm-btn v">form button</button>
+</div> */
